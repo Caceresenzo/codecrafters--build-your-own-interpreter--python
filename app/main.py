685f8e5a5363b1185997b260be260e0e35d102fd
Caceresenzo <caceresenzo1502@gmail.com>
2024-07-06 21:47:04 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,49 @@
 import sys
+import typing
+
+from .grammar import Token, TokenType
+
+
+class Scanner:
+
+    def __init__(
+        self,
+        source: str
+    ):
+        self.source = source
+        self.tokens: typing.List[Token] = []
+
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+    @property
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+    def scan_tokens(self):
+        while not self.is_at_end:
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def scan_token(self):
+        character = self.advance()
+
+        match character:
+            case '(': self.add_token(TokenType.LEFT_PAREN)
+            case ')': self.add_token(TokenType.RIGHT_PAREN)
+
+    def advance(self):
+        index = self.current
+        self.current += 1
+        return self.source[index]
+
+    def add_token(self, type: TokenType, literal: typing.Any = None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
 
 
 def main():
@@ -16,10 +61,15 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    if file_contents:
-        raise NotImplementedError("Scanner not implemented")
-    else:
-        print("EOF  null")  # Placeholder, remove this line when implementing the scanner
+    scanner = Scanner(file_contents)
+    tokens = scanner.scan_tokens()
+
+    for token in tokens:
+        literal = token.literal
+        if literal is None:
+            literal = "null"
+
+        print(f"{token.type.name} {token.lexeme} {literal}")
 
 
 if __name__ == "__main__":
