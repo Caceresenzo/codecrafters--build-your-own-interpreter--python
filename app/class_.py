25de3c65adb1fbf78aff6368764a34bce32119ef
Caceresenzo <caceresenzo1502@gmail.com>
2025-03-16 22:52:27 +0100

    pass 2nd classes

--- app/class_.py
+++ app/class_.py
@@ -1,7 +1,25 @@
-class LoxClass:
+from .function import Callable
+
+
+class LoxClass(Callable):
 
     def __init__(self, name: str):
         self.name = name
 
+    def arity(self):
+        return 0
+
+    def call(self, interpreter, arguments):
+        return LoxInstance(self)
+
     def __str__(self):
         return self.name
+
+
+class LoxInstance:
+
+    def __init__(self, klass: LoxClass):
+        self.klass = klass
+
+    def __str__(self):
+        return f"{self.klass.name} instance"
